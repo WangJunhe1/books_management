f91e704443c67033446d811ad8f705a2c38c9b49
王珺禾 <1275545204@stu.huel.edu.cn>
2023-12-27 20:20:46 +0800

    token修正

---
+++ backEnd/src/main/java/com/seven/exception/TokenNotExitException.java
@@ -0,0 +1,15 @@
+package com.seven.exception;
+
+/**
+ * @author :Wjh
+ * @since :2023/12/27 19:52
+ */
+
+public class TokenNotExitException extends BaseException{
+    public TokenNotExitException() {
+    }
+
+    public TokenNotExitException(String msg) {
+        super(msg);
+    }
+}
